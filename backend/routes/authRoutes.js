a935abb52267956dd2b7d4eb8739642dc6d77c22
Rebel Soul <mudvari.ankit@gmail.com>
2020-07-11 02:53:03 -0400

    addded error handling for signup route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const db = require('../models');
 
-router.post('/signup', (req, res) => {
-  console.log(req.body);
-
+router.post('/signup', async (req, res) => {
+  //   console.log(req.body);
   const {
     firstName,
     lastName,
@@ -15,20 +14,25 @@ router.post('/signup', (req, res) => {
     email,
     password,
   } = req.body;
-  const user = new db.Groomer({
-    firstName,
-    lastName,
-    street,
-    city,
-    state,
-    zip,
-    phone,
-    email,
-    password,
-  });
-  user.save();
 
-  res.send(`Hello ${email}`);
+  try {
+    const groomer = new db.Groomer({
+      firstName,
+      lastName,
+      street,
+      city,
+      state,
+      zip,
+      phone,
+      email,
+      password,
+    });
+    await groomer.save();
+    return res.send(`${email} sucessfully signed up`);
+  } catch (err) {
+    console.error(err);
+    return res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
