8d8c9793399ae14a45292f9b2aae28d10dcd5a2b
Dean Warren <deawar@gmail.com>
2020-08-01 17:32:55 -0400

    Email Sends

--- backend/routes/EmailVerification.js
+++ backend/routes/EmailVerification.js
@@ -1,9 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const os = require('os');
 const db = require('../models');
-const router = require('express').Router();
-const authToken = require('../config/authToken');
+
 require('dotenv').config(); // move to a dev-dependency must run "node -r dotenv/config server.js"
 // or "npm run start_local"
 const smtpTransport = require('../config/verify'); // { sendMail }
@@ -15,56 +12,42 @@ const PORT = process.env.PORT || 3000;
 // Email verification
 let mailOptions;
 let link;
-let secretToken = authToken;
-// user.value.secretToken = secretToken;
-// user.value.active = false; // Flag account as inactive until verified
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
 
-const emailverify = function (userToken, email) {
-  router.post('/send', authToken, (req, res) => {
-    console.log('Line 29 in Send route', req.body);
-    if (req.isAuthenticated()) {
-      db.Groomer.findOne({ _id: req.params.id })
-        .then((dbUser) => {
-          const user = {
-            userInfo: dbUser.dataValues,
-            id: req.session.passport.user,
-            secretToken: dbUser.secretToken,
-            isloggedin: req.isAuthenticated(),
-          };
-          console.log('Line 79 User.Info:', user.userInfo);
-          console.log('Line 86 os.hostname(): ', os.hostname());
-          res.send(user.secretToken);
-          secretToken = user.secretToken;
-        })
-        .then(() => {
-          // eslint-disable-next-line no-cond-assign
-          if (process.env.NODE_ENV === 'development') {
-            link = `http://${hostname}:${PORT}/Verify?id=${secretToken}`;
-          } else {
-            link = `https://localhost:${PORT}/Verify?id=${secretToken}`;
-          }
-          console.log('Verify Return Link: ', link);
-          mailOptions = {
-            from: '"InstaGroomMe" <instagroomme@gmail.com>',
-            to: req.body.to,
-            subject:
-              'InstaGromMe is asking you to confirm your Email account',
-            // eslint-disable-next-line prefer-template
-            html: `<div itemscope itemtype="http://schema.org/EmailMessage">
-            <div itemprop="potentialAction" itemscope itemtype="http://schema.org/ConfirmAction">
-              <meta itemprop="name" content="Verify Email"/>
-              <div>
-                <p>Hi there,<br> Copy this token:<br><b>${secretToken}</b><br>and paste it into the Verification page at the link below.<br>
-                Please Click on the link to verify your email. <br><a href=${link}>Click here to verify</a></p>
-              <div itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler">
-                <link itemprop="url" href="${link}"/>
+const emailverify = function (id) {
+  db.Groomer.findOne({ _id: id })
+    .then((dbUser) => {
+      const user = {
+        id: dbUser._id,
+        email: dbUser.email,
+        secretToken: dbUser.userToken,
+        isVerified: dbUser.isVerified,
+      };
+      console.log('***DBUser***:', user);
+      if (process.env.NODE_ENV === 'development') {
+        link = `http://${hostname}:${PORT}/api/verify/Bearer%20${user.secretToken}`;
+      } else {
+        link = `https://localhost:${PORT}/api/verify/Bearer%20${user.secretToken}`;
+      }
+      console.log('Verify Return Link: ', link);
+      mailOptions = {
+        from: '"InstaGroomMe" <instagroomme@gmail.com>',
+        to: user.email,
+        subject:
+                'InstaGroomMe is asking you to confirm your Email account',
+        // eslint-disable-next-line prefer-template
+        html: `<div itemscope itemtype="http://schema.org/EmailMessage">
+              <div itemprop="potentialAction" itemscope itemtype="http://schema.org/ConfirmAction">
+                <meta itemprop="name" content="Verify Email"/>
+                <div>
+                  <p>Hi there,<br> 
+                  Please Click on the link to verify your email. <br><a href=${link}>Click here to verify</a></p>
+                <div itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler">
+                  <link itemprop="url" href="${link}"/>
+                </div>
               </div>
+              <meta itemprop="description" content="Email Verification Request"/>
             </div>
-            <meta itemprop="description" content="Email Verification Request"/>
-          </div>
-          
+
           <script type="application/ld+json">
           {
             "@context": "https://instagroom.me/",
@@ -74,121 +57,116 @@ const emailverify = function (userToken, email) {
               "name": "Approve Expense",
               "handler": {
                 "@type": "HttpActionHandler",
-                "url": "https://instagroom.me/verify?id=${secretToken}"
+                "url": "https://instagroom.me/api/verify/Bearer%20${user.secretToken}"
               }
             },
-            "description": "Email Verification for Silent Auction Gallery"
+            "description": "Email Verification for Instagroom.Me"
           }
           </script>`,
-            // html: `Hi there,<br> Copy this token:<br><b>${secretToken}</b>
-            // <br>and paste it into the Verification page at the link below.<br>
-            // Please Click on the link to verify your email. <br><a href=${link}>
-            // Click here to verify</a>`,
-          };
-          console.log('Sent by:', process.env.GMAIL_USERNAME);
-          console.log('Line 87 signup_controller.js: ', mailOptions);
-          // eslint-disable-next-line func-names
-          // eslint-disable-next-line no-unused-vars
-          smtpTransport.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              console.log('Error happened!!!');
-              res.status(500).json({ message: 'Error happened!!' });
-            } else {
-              console.log('Email sent!!!');
-              res.json({ message: 'Email sent!!' });
-            }
-          });
-        });
-    } else {
-      // eslint-disable-next-line no-unused-vars
-      const user = {
-        id: null,
-        isloggedin: req.isAuthenticated(),
+        // html: `Hi there,<br> Copy this token:<br><b>${secretToken}</b>
+        // <br>and paste it into the Verification page at the link below.<br>
+        // Please Click on the link to verify your email. <br><a href=${link}>
+        // Click here to verify</a>`,
       };
-      res.redirect('/');
-    }
-  });
-}
-secretToken = ''; // to clear for verify
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
-
-router
-  // eslint-disable-next-line no-unused-vars
-  .get('/verify', (req, res, next) => {
-    console.log('<----------------------------------Req.body: ', req.body);
-    res.render('verifytoken', { title: 'Verify Email Page' });
-  })
-  // eslint-disable-next-line prefer-template
-  // console.log(req.protocol + ':/' + req.get('host'));
-  // eslint-disable-next-line prefer-template
-  // TODO: change this to a promis Referenece: https://zellwk.com/blog/async-await-express/
-  .post('/verify', async (req, res, next) => {
-    try {
-      secretToken = req.body;
-
-      console.log('Line 182 ----->secretToken:', secretToken);
-      // Find account with matching secret Token
-      const user = await db.User.findOne({
-        where: {
-          secretToken: secretToken.secretToken,
-        },
-      });
-      if (!user.dataValues.secretToken || user.dataValues.active === 1 || user.dataValues.secretToken === ' ') {
-        req.flash('You have either already confirmed your account OR you may need to register');
-        return res.status(404).redirect('/signup', { title: 'Register Page' });
-      }
-      console.log('Line 193------->User db output:', user.dataValues.secretToken);
-      console.log('line 194 ------>User db active output:', user.dataValues.active);
-
-      if (user.dataValues.secretToken === secretToken.secretToken) {
-        console.log('Domain is matched. Information is from Authentic email. secretToken:',
-          req.query.id === secretToken);
-        console.log('email is verified');
-        console.log('In Verify Route and user: ', user);
-        if (!user) {
-          console.log('*****************User NOT Found!!!****************');
-          // res.;
-          req.flash('Error, No user found.');
-          res.status(401).redirect('/signup');
-          return;
+      console.log('Sent by:', process.env.GMAIL_USERNAME);
+      console.log('Line 81 signup_controller.js: ', mailOptions);
+      // eslint-disable-next-line func-names
+      // eslint-disable-next-line no-unused-vars
+      smtpTransport.sendMail(mailOptions, (error, info) => {
+        console.log('Information!!!', info);
+        if (error) {
+          console.log('Error happened!!!', error);
+          console.log('Information!!!', info);
+          // res.status(500).json({ message: 'Error happened!!' });
+        } else {
+          console.log('Email sent!!!');
+          // res.json({ message: 'Email sent!!' });
         }
-        const condition = {
-          where: {
-            secretToken: secretToken.secretToken,
-          },
-        };
-        console.log('Condition----->: ', condition);
-        db.User.update({ secretToken: null, active: true }, condition)
-            .then((result) => {
-                console.log('============>', result);
-                if (result.changedRows > 0) {
-                    res.json({
-                        error: false,
-                        data: singleCustomer,
-                        message: `Success! Thank you, please login.`,
-                    });
-                    res.redirect('/login').status(200),
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json({
-                error: true,
-                data: null,
-                message: 'No Customer with such id found.',
-                });
-                res.redirect('/signup');
-            });
-      } else {
-        req.flash('Success', 'Thank you! Now you can Login.');
-        res.redirect('/login');
-      }
-    } catch (error) {
-      // throw new Error('BROKEN-DID NOT CATCH THE NULL VALUE')
-      // eslint-disable-next-line no-unreachable
-      return next(error);
-    }
-  });
+      });
+    });
+};
+// eslint-disable-next-line no-cond-assign
 
-  export verifyEmail();
+// user.secretToken = ''; // to clear for verify
+// router.use(bodyParser.urlencoded({ extended: true }));
+// router.use(bodyParser.json());
+
+// router
+//   // eslint-disable-next-line no-unused-vars
+//   .get('/verify', (req, res, next) => {
+//     console.log('<----------------------------------Req.body: ', req.body);
+//     res.render('verifytoken', { title: 'Verify Email Page' });
+//   })
+//   // eslint-disable-next-line prefer-template
+//   // console.log(req.protocol + ':/' + req.get('host'));
+//   // eslint-disable-next-line prefer-template
+//   // TODO: change this to a promis Referenece: https://zellwk.com/blog/async-await-express/
+//   .post('/verify', async (req, res, next) => {
+//     try {
+//       secretToken = req.body;
+
+//       console.log('Line 182 ----->secretToken:', secretToken);
+//       // Find account with matching secret Token
+//       const user = await db.User.findOne({
+//         where: {
+//           secretToken: secretToken.secretToken,
+//         },
+//       });
+//       if (!user.dataValues.secretToken || user.dataValues.active === 1 || user.dataValues.secretToken === ' ') {
+//         req.flash('You have either already confirmed your account OR you may need to register');
+//         return res.status(404).redirect('/signup', { title: 'Register Page' });
+//       }
+//       console.log('Line 193------->User db output:', user.dataValues.secretToken);
+//       console.log('line 194 ------>User db active output:', user.dataValues.active);
+
+//     if (user.dataValues.secretToken === secretToken.secretToken) {
+//       console.log('Domain is matched. Information is from Authentic email. secretToken:',
+//         req.query.id === secretToken);
+//       console.log('email is verified');
+//       console.log('In Verify Route and user: ', user);
+//       if (!user) {
+//         console.log('*****************User NOT Found!!!****************');
+//         // res.;
+//         req.flash('Error, No user found.');
+//         res.status(401).redirect('/signup');
+//         return;
+//       }
+//       const condition = {
+//         where: {
+//           secretToken: secretToken.secretToken,
+//         },
+//       };
+//       console.log('Condition----->: ', condition);
+//       // db.User.update({ secretToken: null, active: true }, condition)
+//       //     .then((result) => {
+//       //         console.log('============>', result);
+//       //         // if (result.changedRows > 0) {
+//       //         //     res.json({
+//       //         //         error: false,
+//       //         //         data: singleCustomer,
+//       //         //         message: `Success! Thank you, please login.`,
+//       //         //     });
+//       //         //     res.redirect('/login').status(200),
+//       //         // }
+//       //     })
+//       //     .catch((err) => {
+//       //         console.log(err);
+//       //         res.status(500).json({
+//       //         error: true,
+//       //         data: null,
+//       //         message: 'No Customer with such id found.',
+//       //         });
+//       //         res.redirect('/signup');
+//       //     });
+//     } else {
+//       req.flash('Success', 'Thank you! Now you can Login.');
+//       res.redirect('/login');
+//     }
+//   } catch (error) {
+//     // throw new Error('BROKEN-DID NOT CATCH THE NULL VALUE')
+//     // eslint-disable-next-line no-unreachable
+//     return next(error);
+//   }
+// };
+
+module.exports = emailverify;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const jwt = require('jsonwebtoken');
 const db = require('../models');
 const authToken = require('../config/authToken');
+const emailverify = require('./EmailVerification');
 
 // Signup Route
 router.post('/signup', async (req, res) => {
@@ -42,11 +43,15 @@ router.post('/signup', async (req, res) => {
       { $set: { userToken: token } },
       { new: true },
     )
-      .then((updatedGroomer) => res.json({
-        error: false,
-        data: { updatedGroomer },
-        message: ' Successfully created new user. ',
-      }));
+      .then((updatedGroomer) => {
+        const verify = emailverify(groomerUser._id);
+        console.log(verify);
+        res.json({
+          error: false,
+          data: { updatedGroomer },
+          message: ' Successfully created new user. ',
+        });
+      });
   } catch (err) {
     return res.status(500).json({
       error: true,
@@ -54,190 +59,12 @@ router.post('/signup', async (req, res) => {
       message: ' An error occurred creating new user. ',
     });
   }
-  // <<<--------preliminary placement for EmailVerification Process--------------------------------------->>>
-  let mailOptions;
-  let link;
-  let secretToken = authToken;
-  // user.value.secretToken = secretToken;
-  // user.value.active = false; // Flag account as inactive until verified
-  router.use(bodyParser.urlencoded({ extended: true }));
-  router.use(bodyParser.json());
-
-  //const emailverify = function (userToken, email) {
-    router.post('/send', authToken, (req, res) => {
-      console.log('Line 29 in Send route', req.body);
-      if (req.isAuthenticated()) {
-        db.Groomer.findOne({ _id: req.params.id })
-          .then((dbUser) => {
-            const user = {
-              userInfo: dbUser.dataValues,
-              id: req.session.passport.user,
-              secretToken: dbUser.secretToken,
-              isloggedin: req.isAuthenticated(),
-            };
-            console.log('Line 79 User.Info:', user.userInfo);
-            console.log('Line 86 os.hostname(): ', os.hostname());
-            res.send(user.secretToken);
-            secretToken = user.secretToken;
-          })
-          .then(() => {
-            // eslint-disable-next-line no-cond-assign
-            if (process.env.NODE_ENV === 'development') {
-              link = `http://${hostname}:${PORT}/Verify?id=${secretToken}`;
-            } else {
-              link = `https://localhost:${PORT}/Verify?id=${secretToken}`;
-            }
-            console.log('Verify Return Link: ', link);
-            mailOptions = {
-              from: '"InstaGroomMe" <instagroomme@gmail.com>',
-              to: req.body.to,
-              subject:
-                'InstaGromMe is asking you to confirm your Email account',
-              // eslint-disable-next-line prefer-template
-              html: `<div itemscope itemtype="http://schema.org/EmailMessage">
-              <div itemprop="potentialAction" itemscope itemtype="http://schema.org/ConfirmAction">
-                <meta itemprop="name" content="Verify Email"/>
-                <div>
-                  <p>Hi there,<br> Copy this token:<br><b>${secretToken}</b><br>and paste it into the Verification page at the link below.<br>
-                  Please Click on the link to verify your email. <br><a href=${link}>Click here to verify</a></p>
-                <div itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler">
-                  <link itemprop="url" href="${link}"/>
-                </div>
-              </div>
-              <meta itemprop="description" content="Email Verification Request"/>
-            </div>
-            
-            <script type="application/ld+json">
-            {
-              "@context": "https://instagroom.me/",
-              "@type": "EmailMessage",
-              "potentialAction": {
-                "@type": "ConfirmAction",
-                "name": "Approve Expense",
-                "handler": {
-                  "@type": "HttpActionHandler",
-                  "url": "https://instagroom.me/verify?id=${secretToken}"
-                }
-              },
-              "description": "Email Verification for Silent Auction Gallery"
-            }
-            </script>`,
-              // html: `Hi there,<br> Copy this token:<br><b>${secretToken}</b>
-              // <br>and paste it into the Verification page at the link below.<br>
-              // Please Click on the link to verify your email. <br><a href=${link}>
-              // Click here to verify</a>`,
-            };
-            console.log('Sent by:', process.env.GMAIL_USERNAME);
-            console.log('Line 87 signup_controller.js: ', mailOptions);
-            // eslint-disable-next-line func-names
-            // eslint-disable-next-line no-unused-vars
-            smtpTransport.sendMail(mailOptions, (error, info) => {
-              if (error) {
-                console.log('Error happened!!!');
-                res.status(500).json({ message: 'Error happened!!' });
-              } else {
-                console.log('Email sent!!!');
-                res.json({ message: 'Email sent!!' });
-              }
-            });
-          });
-      } else {
-        // eslint-disable-next-line no-unused-vars
-        const user = {
-          id: null,
-          isloggedin: req.isAuthenticated(),
-        };
-        res.redirect('/');
-      }
-    });
-  }
-  secretToken = ''; // to clear for verify
-  router.use(bodyParser.urlencoded({ extended: true }));
-  router.use(bodyParser.json());
-
-  router
-    // eslint-disable-next-line no-unused-vars
-    .get('/verify', (req, res, next) => {
-      console.log('<----------------------------------Req.body: ', req.body);
-      res.render('verifytoken', { title: 'Verify Email Page' });
-    })
-    // eslint-disable-next-line prefer-template
-    // console.log(req.protocol + ':/' + req.get('host'));
-    // eslint-disable-next-line prefer-template
-    // TODO: change this to a promis Referenece: https://zellwk.com/blog/async-await-express/
-    .post('/verify', async (req, res, next) => {
-      try {
-        secretToken = req.body;
-
-        console.log('Line 182 ----->secretToken:', secretToken);
-        // Find account with matching secret Token
-        const user = await db.User.findOne({
-          where: {
-            secretToken: secretToken.secretToken,
-          },
-        });
-        if (!user.dataValues.secretToken || user.dataValues.active === 1 || user.dataValues.secretToken === ' ') {
-          req.flash('You have either already confirmed your account OR you may need to register');
-          return res.status(404).redirect('/signup', { title: 'Register Page' });
-        }
-        console.log('Line 193------->User db output:', user.dataValues.secretToken);
-        console.log('line 194 ------>User db active output:', user.dataValues.active);
-
-        if (user.dataValues.secretToken === secretToken.secretToken) {
-          console.log('Domain is matched. Information is from Authentic email. secretToken:',
-            req.query.id === secretToken);
-          console.log('email is verified');
-          console.log('In Verify Route and user: ', user);
-          if (!user) {
-            console.log('*****************User NOT Found!!!****************');
-            // res.;
-            req.flash('Error, No user found.');
-            res.status(401).redirect('/signup');
-            return;
-          }
-          const condition = {
-            where: {
-              secretToken: secretToken.secretToken,
-            },
-          };
-          console.log('Condition----->: ', condition);
-          db.User.update({ secretToken: null, active: true }, condition)
-              .then((result) => {
-                  console.log('============>', result);
-                  if (result.changedRows > 0) {
-                      res.json({
-                          error: false,
-                          data: singleCustomer,
-                          message: `Success! Thank you, please login.`,
-                      });
-                      res.redirect('/login').status(200),
-                  }
-              })
-              .catch((err) => {
-                  console.log(err);
-                  res.status(500).json({
-                  error: true,
-                  data: null,
-                  message: 'No Customer with such id found.',
-                  });
-                  res.redirect('/signup');
-              });
-        } else {
-          req.flash('Success', 'Thank you! Now you can Login.');
-          res.redirect('/login');
-        }
-      } catch (error) {
-        // throw new Error('BROKEN-DID NOT CATCH THE NULL VALUE')
-        // eslint-disable-next-line no-unreachable
-        return next(error);
-      }
-    }
 });
-
 // Verify Signed in Route
 // This route is to retrive the email id or any other info of user by passing token..
 // This is to verify we can get email of user only when valid token is sent..
 router.get('/verify/:authorization', authToken, (req, res) => {
+  console.log(req);
   res.status(200).json({
     error: false,
     data: `${req.groomer.email}`,
